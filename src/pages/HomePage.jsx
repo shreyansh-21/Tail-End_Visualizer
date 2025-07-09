259b44034441cfcd14a0c383f2a25406ea59d60e
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-07-10 01:37:33 +0530

    Add Spline integration to HomePage and update dependencies

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,24 @@
 // src/pages/HomePage.jsx
 import { Link } from "react-router-dom";
+import Spline from '@splinetool/react-spline';
 // import TypewriterHero from "../components/TypewriterHero";
 
 export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center p-8">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Life in Weeks</h1>
-      {/* <TypewriterHero /> */}
-      <Link
-        to="/visualize"
-        className="mt-8 inline-block bg-gray-800 text-white px-6 py-3 rounded-md hover:bg-gray-700 transition"
-      >
-        Visualize Your Time
-      </Link>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center p-8">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">Life in Weeks</h1>
+            <div className="relative overflow-hidden h-95 w-full">
+                <div className="absolute inset-0 -bottom-15">
+                    <Spline scene="https://prod.spline.design/p9YDaDQsLRER8yPq/scene.splinecode" />
+                </div>
+            </div>
+            {/* <TypewriterHero /> */}
+            <Link
+                to="/visualize"
+                className="mt-8 inline-block bg-gray-800 text-white px-6 py-3 rounded-md hover:bg-gray-700 transition"
+            >
+                Visualize Your Time
+            </Link>
+        </div>
+    );
 }
